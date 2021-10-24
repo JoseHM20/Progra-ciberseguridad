53e62f61ccdb9274219d1058b699959a89f47cd4
Jose Luis Hernandez Meza <91032461+JoseHM20@users.noreply.github.com>
2021-10-24 14:38:10 -0500

    Add files via upload

---
+++ Bash/Ejercicio 4/NetScan.sh
@@ -0,0 +1,11 @@
+powershell.netscan.ps1
+
+#!/bin/bash
+function is_alive_ping() {
+         ping -c 1 $1 > /dev/null 2>&1
+         [ $? -eq 0 ] | echo "Node with IP: $i is up."
+}
+for i in 192.168.1.{1..2}
+do
+           is_alive_ping $i
+done
